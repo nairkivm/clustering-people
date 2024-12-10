06b05b6874b8dd6d9f6b58ea5bb4975bdf2c668e
mfikrian-uena <72137289+nairkivm@users.noreply.github.com>
2024-12-10 18:56:00 +0700

    first commit

---
+++ utils/constants.py
@@ -0,0 +1,18 @@
+class Constants:
+    def __init__(self) -> None:
+        self.requirements = {
+            'salary': {
+                'age': 'int64',
+                'workclass': 'object',
+                'education_num': 'int64',
+                'marital_status': 'object',
+                'occupation': 'object',
+                'relationship': 'object',
+                'race': 'object',
+                'sex': 'object',
+                'capital_gain': 'int64',
+                'capital_loss': 'int64',
+                'hours_per_week': 'int64',
+                'native_country': 'object',
+            },
+        }
